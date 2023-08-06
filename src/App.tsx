3c10152c186ce0b882455af243afc59215ca701d
Peter Schmidt-Nielsen <schmidtnielsenpeter@gmail.com>
2023-08-06 14:00:57 -0700

    Make the beginning harder.

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import { ILayoutResult, Rescaler } from './Rescaler';
 import { Point, interpolate, dist, rotate, turnTowards } from './Interpolate';
 
-const VERSION = 'v0.23';
+const VERSION = 'v0.24';
 const WIDTH = 1600;
 const HEIGHT = 1000;
 const CELL_SIZE = 50;
@@ -810,7 +810,7 @@ class App extends React.PureComponent<IAppProps> {
   startWave = () => {
     if (this.gameState !== 'build')
       return;
-    this.gold += 75;
+    this.gold += 50;
     this.gameState = 'wave';
     this.waveTimerMax = 20 + 2.0 * Math.sqrt(this.wave);
     this.waveTimer = 0;
