e3c6a4199eebdaee8525f0c07563841b29e5a6d6
Peter Schmidt-Nielsen <schmidtnielsenpeter@gmail.com>
2024-07-25 23:17:39 -0700

    More adjustments

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import { ILayoutResult, Rescaler } from './Rescaler';
 import { Point, interpolate, dist, rotate, turnTowards } from './Interpolate';
 
-const VERSION = 'v0.80';
+const VERSION = 'v0.81';
 const WIDTH = 1600;
 const HEIGHT = 1000;
 const CELL_SIZE = 50;
@@ -92,7 +92,7 @@ const TURRET_DATA: { [key in TurretType]: TurretData } = {
       },
       {
         name: 'Super Sniper',
-        description: 'Increases range by another 3 tiles, and triples bullet velocity.',
+        description: 'Increases range by another 4 tiles.',
         cost: 350,
       },
       {
@@ -1101,7 +1101,7 @@ class App extends React.PureComponent<IAppProps> {
     if (turret.upgrades.includes('Sniper'))
       range += 3;
     if (turret.upgrades.includes('Super Sniper'))
-      range += 3;
+      range += 4;
     if (turret.upgrades.includes('Range'))
       range += 1.5;
     if (turret.upgrades.includes('Lens'))
