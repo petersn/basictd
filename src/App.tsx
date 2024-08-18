8eb2182d610890416abbabc1b815d0eb74fcfbe0
Peter Schmidt-Nielsen <schmidtnielsenpeter@gmail.com>
2024-08-18 03:07:15 -0700

    Bump version number

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import { ILayoutResult, Rescaler } from './Rescaler';
 import { Point, interpolate, dist, rotate, turnTowards } from './Interpolate';
 
-const VERSION = 'v0.91';
+const VERSION = 'v0.92';
 const WIDTH = 1600;
 const HEIGHT = 1000;
 const CELL_SIZE = 50;
