8189deffe4c3335b0626e22b04544c658b48705b
Peter Schmidt-Nielsen <schmidtnielsenpeter@gmail.com>
2024-08-18 00:22:12 -0700

    Let's make the bullets much bigger

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import { ILayoutResult, Rescaler } from './Rescaler';
 import { Point, interpolate, dist, rotate, turnTowards } from './Interpolate';
 
-const VERSION = 'v0.90';
+const VERSION = 'v0.91';
 const WIDTH = 1600;
 const HEIGHT = 1000;
 const CELL_SIZE = 50;
@@ -1176,7 +1176,7 @@ class App extends React.PureComponent<IAppProps> {
       return [b];
     }
 
-    let speed = 925.0;
+    let speed = 900.0;
     if (turret.upgrades.includes('Sniper'))
       speed *= 3.0;
     const bullets = [
@@ -1200,7 +1200,7 @@ class App extends React.PureComponent<IAppProps> {
       }
     }
     for (const bullet of bullets) {
-      bullet.size *= 1.35;
+      bullet.size *= 1.6;
       bullet.damage = data.damage;
       if (turret.upgrades.includes('Piercing')) {
         bullet.hp += 1;
