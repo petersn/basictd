dd9a4cbc54cf4b7fa90f7a9c01512a967bfa8281
Peter Schmidt-Nielsen <schmidtnielsenpeter@gmail.com>
2024-09-05 02:53:46 -0700

    Whoopsies

--- src/App.tsx
+++ src/App.tsx
@@ -1148,7 +1148,7 @@ class App extends React.PureComponent<IAppProps> {
       range += 2;
     if (this.cheatTurboRange) {
       range *= 1.5;
-      if (turret.type === 'Repair Station') {
+      if (turret.type === 'repair') {
         range = Math.ceil(range);
       }
     }
